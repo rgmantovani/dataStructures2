af16fee1f5494671647ce4e858a0af8ef7c521b4
Rafael Gomes Mantovani <rgmantovani@gmail.com>
2021-10-05 22:55:07 -0300

    adding bubble sort content

---
+++ codes/01_sorts/bubbleSort.c
@@ -0,0 +1,11 @@
+/* Ordena o vetor usando BubbleSort
+Parâmetros:
+   v: vetor a ser ordenado 
+   n: número de elemento do vetor, tamanho do vetor
+
+Esse algoritmo tem um comportamento assindético O(N2)
+ */
+
+void bubbleSort(int *v, int n) {
+ // add lógica aqui
+}
