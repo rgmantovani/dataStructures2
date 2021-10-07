804035d34f26bab409e06712c1fd807d87699bd9
Rafael Gomes Mantovani <rgmantovani@gmail.com>
2021-10-06 23:39:07 -0300

    adding bubblesort

---
+++ codes/02_sorts/sorts.h
@@ -0,0 +1,10 @@
+#ifndef sorts_h
+#define sorts_h
+
+#include <stdio.h>
+#include <stdbool.h>
+
+void printArray(int *v, int tam);   // imprime vetor
+void bubbleSort(int *v, int n);     //Bubble sort
+
+#endif /* sorts_h */
