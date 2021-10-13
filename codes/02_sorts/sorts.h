0278cb631b135808f4b6cec574c93fb1fdf94f52
Rafael Gomes Mantovani <rgmantovani@gmail.com>
2021-10-13 15:21:00 -0300

    updating sorts

--- codes/02_sorts/sorts.h
+++ codes/02_sorts/sorts.h
@@ -4,7 +4,9 @@
 #include <stdio.h>
 #include <stdbool.h>
 
-void printArray(int *v, int tam);   // imprime vetor
-void bubbleSort(int *v, int n);     //Bubble sort
+void printArray(int *v, int tam);   // Print an array
+void bubbleSort(int *v, int n);     // Bubble sort
+void insertionSort(int *v, int n);  // Insertion Sort
+void selectionSort(int *v, int n);  // Selection Sort
 
 #endif /* sorts_h */
