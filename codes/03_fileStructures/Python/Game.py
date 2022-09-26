9d2b725f9e0573ff3e7d0afbb9f26cd275f5e3ae
Rafael Gomes Mantovani <rgmantovani@gmail.com>
2022-09-26 10:30:01 -0300

    updating Game.py

--- codes/03_fileStructures/Python/Game.py
+++ codes/03_fileStructures/Python/Game.py
@@ -1,6 +1,9 @@
-# --------------------------------
-# --------------------------------
+# --------------------------------------------
+# Classe Game: representação de um jogo (game)
+# --------------------------------------------
+
 class Game:
+
     # construtor do objeto Game
     def __init__(self, nome=None, genero=None, plataforma=None,
         ano=None, classificacao=None, preco=None, midia=None,
@@ -20,6 +23,54 @@ class Game:
 
     def getNome(self):
         return (self.nome)
+
+    def setGenero(self, genero):
+        self.genero = genero
+
+    def getGenero(self):
+        return (self.genero)
+
+    def setPlataforma(self, plataforma):
+        self.plataforma = plataforma
+
+    def getPlataforma(self):
+        return (self.plataforma)
+
+    def setAno(self, ano):
+        self.ano = ano
+
+    def getAno(self):
+        return (ano)
+
+    def setClassificacao(self, classificacao):
+        self.classificacao = classificacao
+
+    def getClassificacao(self):
+        return (self.classificacao)
+
+    def setPreco(self, preco):
+        self.preco = preco
+
+    def getPreto(self):
+        return (preco)
+
+    def setMidia(self, midia):
+        self.midia = midia
+
+    def getMidia(self):
+        return (self.midia)
+
+    def setTamanho(self, tamanho):
+        self.tamanho = tamanho
+
+    def getTamanho(self):
+        return (self.tamanho)
+
+    def setProdutora(self, produtora):
+        self.produtora = produtora
+
+    def getProdutora(self):
+        return (self.produtora)
 
 # --------------------------------
 # --------------------------------
