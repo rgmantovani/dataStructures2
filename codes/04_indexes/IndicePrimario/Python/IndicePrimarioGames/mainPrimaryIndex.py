fe4be973ff2480f3ef69b1c31d4ddae311aea2c3
Rafael Gomes Mantovani <rgmantovani@gmail.com>
2023-11-19 16:00:28 -0300

    adding external content

--- codes/04_indexes/IndicePrimario/Python/IndicePrimarioGames/mainPrimaryIndex.py
+++ codes/04_indexes/IndicePrimario/Python/IndicePrimarioGames/mainPrimaryIndex.py
@@ -8,10 +8,9 @@ from primaryIndex import IndicePrimario
 #------------------------------------------------------
 
 if __name__ == "__main__":
-	print("oi")
 
+    # construtor 
 	estrutura = IndicePrimario()
-	# construtor 
 	
 	# existe a chave
 	res = estrutura.pesquisaRegistro(chave="HADES2019")
@@ -25,7 +24,6 @@ if __name__ == "__main__":
 	estrutura.insereRegistro(newRegistro)
 
 	#estrutura.removeRegistro(chave)
-
 	# destruir (chamado implicitamente pelo python)
 
 
